7324b0a294f8e4fe1353cb3133ca09f4f2ac4014
tommy-yoon <taeil.yoon@gmail.com>
2021-11-07 18:00:52 +1300

    add supertest with cheerio

--- server/routes/example.js
+++ server/routes/example.js
@@ -2,7 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.render('example', { message: 'example message for testing' })
+  res.render('example')
+})
+
+router.get('/param/:param', function (req, res, next) {
+  const param = req.params.param
+  res.render('example', { message: 'example message for testing', param: param })
+})
+
+router.get('/query', function (req, res, next) {
+  const queryValue = req.query.key
+  res.render('example', { message: 'key', param: queryValue })
+})
+
+router.post('/posttest', function (req, res, next) {
+  const { userId, content } = req.body
+
+  res.render('example', { message: `Hello from userID:${userId}`, param: `The content:${content}` })
 })
 
 module.exports = router
--- server/tests/example.test.js
+++ server/tests/example.test.js
@@ -12,8 +12,34 @@ test('Example - testing route with supertest', async () => {
 })
 
 test('Example - testing route with supertest and cheerio', async () => {
-  const res = await request(server).get('/example')
+  const res = await request(server)
+    .get('/example/param/123')
+
   expect(res.statusCode).toEqual(200)
   const $ = cheerio.load(res.text)
   expect($('#msg').text()).toMatch('example message for testing')
+  expect($('#param').text()).toMatch('123')
+})
+
+test('Example - sending query string', async () => {
+  const res = await request(server)
+    .get('/example/query')
+    .query({ key: 'value' })
+
+  expect(res.statusCode).toEqual(200)
+  const $ = cheerio.load(res.text)
+  expect($('#msg').text()).toMatch('key')
+  expect($('#param').text()).toMatch('value')
+})
+
+test('Example POST - sending form data', async () => {
+  const formData = { userId: 101, content: 'Testing POST' }
+  const res = await request(server)
+    .post('/example/posttest/')
+    .send(formData)
+
+  expect(res.statusCode).toEqual(200)
+  const $ = cheerio.load(res.text)
+  expect($('#msg').text()).toMatch(`Hello from userID:${formData.userId}`)
+  expect($('#param').text()).toMatch(`The content:${formData.content}`)
 })
