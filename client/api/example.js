f5a3755762e65b1eea6833cbf6ae9bccdc04c161
tommy-yoon <taeil.yoon@gmail.com>
2021-11-23 16:22:50 +1300

    fix the API url in client side

--- client/api/example.js
+++ client/api/example.js
@@ -2,7 +2,7 @@ import request from 'superagent'
 
 export async function getExamples () {
   try {
-    const data = await request('/example/api/')
+    const data = await request('/examples/api/')
     return data.body.examples
   } catch (error) {
     return []
