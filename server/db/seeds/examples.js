1a3f08efda330a082a0576f87e8ac0f75d7d37ae
tommy-yoon <taeil.yoon@gmail.com>
2021-11-27 20:05:23 +1300

    change the title

--- server/db/seeds/examples.js
+++ server/db/seeds/examples.js
@@ -6,7 +6,7 @@ exports.seed = function (knex) {
       return knex('examples').insert([
         { id: 1, count: 3, title: 'First', description: 'First row' },
         { id: 2, count: 4, title: 'Second', description: 'Second row' },
-        { id: 3, count: 20, title: 'Third', description: 'Third row' }
+        { id: 3, count: 20, title: 'Third', description: 'Third row!' }
       ])
     })
 }
