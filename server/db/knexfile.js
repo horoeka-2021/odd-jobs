530638a254fbeb4c3dd98a4a403ba9b4db89e84b
tommy-yoon <taeil.yoon@gmail.com>
2021-11-29 15:12:56 +1300

    set local and development environment separately

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
 
-  development: {
+  local: {
     client: 'sqlite3',
     connection: {
       filename: path.join(__dirname, 'dev.sqlite3')
@@ -20,6 +20,22 @@ module.exports = {
     },
     seeds: {
       directory: './server/db/seeds'
+    },
+    useNullAsDefault: true
+  },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     },
     useNullAsDefault: true
   },
