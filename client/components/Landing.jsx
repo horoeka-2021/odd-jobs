b444f70a9217e6268f90af13aa2456bb1cd17eb3
Nicky Caparas <nicky@nickycaparas.com>
2021-12-03 23:23:39 +1300

    Finish Landing Page Styling

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -25,32 +25,41 @@ function Landing (props) {
 
   return (
     <div className="flex mb-4">
-      <div className="w-5/12 bg-gray-400 p-5">
+      <div className="w-5/12 pl-10 pr-10 pt-10 mx-20">
         <h2 className="text-5xl font-weight: 700 font-bold">Get the Job</h2>
         <h2 className="text-5xl font-weight: 700 font-bold">Done In A Day</h2>
-        <h3>
+        <p className="text-justify leading-tight w-4/5 pt-3">
               Gain experience and be mentored by the best
-              in the industry. Start your training, make
+              in the industry.Start your training, make
               a difference, and enhance your skills today!
-        </h3>
-        <div className="w-3/4 grid grid-cols-2 pt-5 pb-10 bg-blue-200">
-          <div>
-            <h3>APPRENTICE</h3>
-            <p>Prove your capability and skill and get the job done</p>
-            <Link to='/apprentice'>
-              <button className="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">GET STARTED</button>
-            </Link>
+        </p>
+        <div className="w-full grid grid-cols-2 gap-2 pt-10 pb-5">
+          <div className="w-44">
+            <h3 className="text-xl font-weight: 700 font-bold">APPRENTICE</h3>
+            <p className="leading-tight">Prove your capability and skill and get the job done</p>
+
           </div>
+
           <div>
-            <h3>COMMUNITY MEMBER</h3>
-            <p>Post your requirements and match it with the right one</p>
+            <h3 className="text-xl font-weight: 700 font-bold">COMMUNITY MEMBER</h3>
+            <p className="leading-tight">Post your requirements and match it with the right one</p><br></br>
+          </div>
+
+          <div className="flex items-baseline">
             <Link to='/member'>
               <button className="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">GET STARTED</button>
             </Link>
           </div>
+
+          <div className="flex items-baseline">
+            <Link to='/apprentice'>
+              <button className="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">GET STARTED</button>
+            </Link>
+          </div>
+
         </div>
       </div>
-      <div className="flex flex-col justify-end items-end">
+      <div className="mx-10 flex flex-col justify-end items-end">
         <img className="construct-image" src="../img/construction.gif" />
       </div>
     </div>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -17,18 +17,18 @@ function Nav () {
   }
 
   return (
-    <div className="navbar mb-2 bg-red-200 text-neutral-content">
-      <div className="flex-none pr-4 pl-2 mx-2">
-        <img className="max-h-24" src="../img/oddjob_400_200px.png" />
-        <div className="flex-row px-2 mx-40">
-          <div className="items-stretch hidden lg:flex">
-            <Link to='/' className="btn btn-ghost text-gray-600 btn-sm rounded-btn font-sans mx-2">Home</Link>
-            <Link to='/' className="btn btn-ghost  text-gray-600 btn-sm rounded-btn font-sans mx-2">Our Story</Link>
-            <Link to='/' className="btn btn-ghost text-gray-600 btn-sm rounded-btn font-sans mx-2">Hire Now</Link>
-            <Link to='/jobs' className="btn btn-ghost text-gray-600 btn-sm rounded-btn font-sans mx-2">Find Jobs</Link>
-          </div>
+    <div className="navbar mb-2 bg-white text-neutral-content w-full">
+      <div className="flex flex-row w-full">
+        <div className="px-15">
+          <img className="max-h-24" src="../img/oddjob_400_200px.png" />
         </div>
-        <div className="flex-row content-end lg:flex align">
+        <div className="flex flex-1 content-between justify-evenly">
+          <Link to='/' className="btn btn-ghost text-gray-600 btn-m rounded-btn font-sans mx-2">Home</Link>
+          <Link to='/' className="btn btn-ghost  text-gray-600 btn-m rounded-btn font-sans mx-2">Our Story</Link>
+          <Link to='/' className="btn btn-ghost text-gray-600 btn-m rounded-btn font-sans mx-2">Hire Now</Link>
+          <Link to='/jobs' className="btn btn-ghost text-gray-600 btn-m rounded-btn font-sans mx-2">Find Jobs</Link>
+        </div>
+        <div className="flex flex-row items-end">
           <IfAuthenticated>
             <Link to='/' className="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600" onClick={handleLogOff}>Log off</Link>
           </IfAuthenticated>
