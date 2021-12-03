4f19b123c8348872330e4188769cd93f841f2c34
Nicky Caparas <nicky@nickycaparas.com>
2021-12-03 19:59:15 +1300

    Updated sqlite3 version 5

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -24,7 +24,6 @@ function Landing (props) {
   }
 
   return (
-<<<<<<< HEAD
     <div className="flex mb-4">
       <div className="w-5/12 bg-gray-400 p-5">
         <h2 className="text-5xl font-weight: 700 font-bold">Get the Job</h2>
@@ -54,51 +53,6 @@ function Landing (props) {
       <div className="flex flex-col justify-end items-end">
         <img className="construct-image" src="../img/construction.gif" />
       </div>
-||||||| 917bcbe
-    <div>
-      <h1>New Zealand&#39;s leading odd jobs platform for local community</h1>
-      <h2>Connect with people to get the job done on odd Jobs</h2>
-      <h3>Get Started Now</h3>
-
-      <p>Want your home cleaned or furniture put together? Just tell us about the task you’d like done, suggest a fair budget for a job well done and you’ll start to receive offers from available Taskers.</p>
-
-      <h3>How does Odd Jobs work?</h3>
-      <ul>
-        <li>Post your Task</li>
-        <ul><li>Tell us what you need. Its FREE to post!</li></ul>
-        <li>Review offers</li>
-        <ul><li>Get offers from Apprentice memebr and view profiles</li></ul>
-        <li>Get it done</li>
-        <ul><li>Choose the right person for your task and get it done</li></ul>
-
-      </ul>
-
-      <Link to='/apprentice'><button>Apprentices</button></Link>
-      <Link to='/member'><button>Community Member</button></Link>
-
-=======
-    <div>
-      <h1>New Zealand&#39;s leading odd jobs platform for local community</h1>
-      <h2>Connect with people to get the job done on odd Jobs</h2>
-      <h3>Get Started Now</h3>
-
-      <p>Want your home cleaned or furniture put together? Just tell us about the task you’d like done, suggest a fair budget for a job well done and you’ll start to receive offers from available Taskers.</p>
-
-      <h3>How does Odd Jobs work?</h3>
-      <ul>
-        <li>Post your Task</li>
-        <ul><li>Tell us what you need. Its FREE to post!</li></ul>
-        <li>Review offers</li>
-        <ul><li>Get offers from Apprentice memebr and view profiles</li></ul>
-        <li>Get it done</li>
-        <ul><li>Choose the right person for your task and get it done</li></ul>
-
-      </ul>
-
-      <Link to='/apprentice/new'><button>Apprentices</button></Link>
-      <button onClick={handleMember}> Community Member</button>
-
->>>>>>> dev
     </div>
 
   )
