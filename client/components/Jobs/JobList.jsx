c954e29d5dc153ff8ccb98321be49ff666e2d41e
Nicky Caparas <nicky@nickycaparas.com>
2021-12-06 22:56:04 +1300

    Added  Job lists button

--- client/components/Jobs/JobList.jsx
+++ client/components/Jobs/JobList.jsx
@@ -9,28 +9,30 @@ function Jobs (props) {
   console.log('job list', path)
   console.log('joblist', jobs, userID) // memebr
   const [showDetail, setShowDetail] = useState(false)
-  
 
-  function handleDetail () {
-    console.log('show')
-    setShowDetail(true)
-  }
+  //   function handleDetail () {
+  //     // console.log('show')
+  //     // setShowDetail(true)
+  //   }
   /// get by user
   // all job unde this user - 4
   // id to id
 
   return (
-    <div>
 
-      <h3>You have listed {jobs.length} jobs</h3>
+    <div className="listing-tab">
+      <h3 className="text-2xl">You have listed {jobs.length} jobs</h3>
 
-      {jobs.map((data) => {
+      {jobs.map((data, index) => {
         return (
-          <div key={data.jobsId}>
+          <div key={data.jobsId} tabIndex={index} className="collapse w-96 border rounded-box border-base-300">
+            <div className="collapse-title text-m font-medium">
+              {data.jobsId} - {data.jobsTitle}
+            </div>
 
-            <p>{data.jobsId}</p>
-            <p>{data.jobsTitle}</p>
-            <button onClick={handleDetail}>More detail</button>
+            <div className="collapse-content">
+              <p><button>More Details</button></p>
+            </div>
           </div>
         )
       })}
