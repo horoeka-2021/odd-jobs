6b3291f65adba248f12ce3dd512dda9b95f64446
ppc99999 <ppc99999@gmail.com>
2021-12-06 15:29:54 +1300

    merge with front end

--- client/components/Form/AddJob.jsx
+++ client/components/Form/AddJob.jsx
@@ -31,43 +31,71 @@ export default function AddJob (props) {
   return (
     <>
       <div>
-
-        <h2>Add New Job</h2>
+        <h2 className="text-2xl font-semibold">Add New Job</h2>
         <p>New Zealand&#39;s leading odd jobs platform for local community</p>
         <p>It’s amazing what you can’t do yourself</p>
-
       </div>
+
       <section >
         <form >
           <div >
-            <label >Job Title
-              <input name='title' value={newJob.title} onChange={handleChange}/>
-            </label>
-            <label >Job Description
-              <textarea name='description' value={newJob.description }onChange={handleChange}/>
-            </label>
+            <div className="form-control pt-5">
+              <label className="label">
+                <span className="label-text">Job Title</span>
+              </label>
+              <input name='title' value={newJob.title} onChange={handleChange}
+                className="input input-bordered w-3/4"/>
+            </div>
 
-            <label>Classification</label>
-            <select name='service_types' onChange={handleChange}>
-              <option hidden>Select from this list</option>
-              <option value={1}>Plumber</option>
-              <option value={2}>Electrician</option>
-              <option value={3}>Carpenter</option>
-              <option value={4}>Painter</option>
-              <option value={5}>Landscaper</option>
-              <option value={6}>Builder</option>
-              <option value={7}>Gardener</option>
-              <option value={8}>Developer</option>
-              <option value={9}>Mechanic</option>
-              <option value={10}>Tutor</option>
-            </select>
+            <div className="form-control">
+              <label className="label">Job Description
+                <textarea name='description' value={newJob.description }
+                  onChange={handleChange} className="textarea h-24
+              textarea-bordered pr-40 mr-64 mt-4"/>
+              </label>
+            </div>
 
-            <input type="radio" name='payment' value='Paid' onChange={handleChange}/>
-            <label >Paid</label>
-            <input type='radio' name='payment' value='koha' onChange={handleChange}/>
-            <label>Koha</label>
+            <div>
+              <label className="label">Classification</label>
+              <select className="select select-bordered w-full max-w-xs"
+                name='service_types' onChange={handleChange}>
+                <option disabled="disabled" selected="selected">Select from this list</option>
+                <option value={1}>Plumber</option>
+                <option value={2}>Electrician</option>
+                <option value={3}>Carpenter</option>
+                <option value={4}>Painter</option>
+                <option value={5}>Landscaper</option>
+                <option value={6}>Builder</option>
+                <option value={7}>Gardener</option>
+                <option value={8}>Developer</option>
+                <option value={9}>Mechanic</option>
+                <option value={10}>Tutor</option>
+              </select>
+            </div>
+
+            <div className="form-control">
+              <label className="cursor-pointer label justify-start">
+                <input type="radio" name='payment' value='Paid' className="radio" onChange={handleChange}/>
+                <span className="label-text inline">Paid</span>
+              </label>
+            </div>
+
+            <div className="form-control">
+              <label className="cursor-pointer label justify-start">
+                <input type='radio' name='payment' value='koha' className="radio" onChange={handleChange}/>
+                <span className="label-text">Koha</span>
+              </label>
+            </div>
+
           </div>
-          <button onClick={handleAdd}>Add</button>
+
+          <div className="addjob-but">
+            <button className="inline-block px-4 py-3 text-sm
+          font-semibold text-center text-white uppercase transition duration-200 ease-in-out
+         bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600" onClick={handleAdd}>ADD JOB
+            </button>
+          </div>
+
         </form>
       </section>
     </>
