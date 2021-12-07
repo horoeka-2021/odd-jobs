935f72112649950d4dd780241da71517d3aa579c
tommy-yoon <taeil.yoon@gmail.com>
2021-12-07 19:10:11 +1300

    modify table schema -data type for date

--- server/db/migrations/20211202010004_users.js
+++ server/db/migrations/20211202010004_users.js
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary()
+    table.integer('gender_id').notNullable().references('id').inTable('gender')
     table.string('auth0_id').notNullable()
     table.string('name').notNullable()
     table.string('email').notNullable()
     table.string('phone').notNullable()
-    table.integer('birth_date').notNullable()
-    table.integer('gender_id').notNullable().references('id').inTable('gender')
+    table.string('birth_date').notNullable()
   })
 }
 
