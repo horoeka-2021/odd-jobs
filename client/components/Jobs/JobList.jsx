cb75d40e6ddc8ae987b56f2178a74e01f8db178d
ppc99999 <ppc99999@gmail.com>
2021-12-08 04:45:10 +1300

    merge with styling

--- client/components/Jobs/JobList.jsx
+++ client/components/Jobs/JobList.jsx
@@ -25,38 +25,42 @@ function Jobs (props) {
   }, [])
   console.log('joblist api', jobList)
   return (
+    <>
+      {jobList.length === 0 ? <div className="m-10">
+        <h1 className='text-lg font-mono medium text-blue-300'>No Job listed</h1> </div>
+        : <div className="pb-8">
+          <div className="rounded-lg shadow bg-base-200 drawer drawer-mobile h-52">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" placeholder="drawer-toggle"/>
 
-    <div className="pb-8">
-      <div className="rounded-lg shadow bg-base-200 drawer drawer-mobile h-52">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" placeholder="drawer-toggle"/>
+            <div className="drawer-side">
+              <h3 className="drawer-overlay">You have listed {jobs.length} jobs</h3>
 
-        <div className="drawer-side">
-          <h3 className="drawer-overlay">You have listed {jobs.length} jobs</h3>
+              {jobList.map((data, index) => (
 
-          {jobList.map((data, index) => (
+                <ul key={data.jobsId} tabIndex={index} className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                  {/* <div className="collapse-title text-m font-medium"> */}
 
-            <ul key={data.jobsId} tabIndex={index} className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-              {/* <div className="collapse-title text-m font-medium"> */}
+                  <li><button className="text-left" onClick={() => handleDetail(data.jobsId)}>{data.jobsId} - {data.jobsTitle}</button></li>
+                  {/* </div> */}
+                </ul>
 
-              <li><button className="text-left" onClick={() => handleDetail(data.jobsId)}>{data.jobsId} - {data.jobsTitle}</button></li>
-              {/* </div> */}
-            </ul>
+              ))}
+            </div>
 
-          ))}
-        </div>
-
-        {/* <Route exact path={`${path}/details`} >
+            {/* <Route exact path={`${path}/details`} >
         <JobListItem jobID={jobs.jobsID} />
       </Route> */}
 
-        <ul className="drawer-content">
-          { showDetail &&
+            <ul className="drawer-content">
+              { showDetail &&
         <li><JobListItem jobID={jobID}/></li>
-          }
-        </ul>
+              }
+            </ul>
 
-      </div>
-    </div>
+          </div>
+        </div>
+      }
+    </>
   )
 }
 
