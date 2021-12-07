90c3019601ddabbce2df8a296bdce4ea0de27f0a
tommy-yoon <taeil.yoon@gmail.com>
2021-12-07 19:17:16 +1300

    modify table schema -data type for date

--- server/db/migrations/20211202010004_users.js
+++ server/db/migrations/20211202010004_users.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
     table.string('name').notNullable()
     table.string('email').notNullable()
     table.string('phone').notNullable()
-    table.string('birth_date').notNullable()
+    table.date('birth_date').notNullable()
   })
 }
 
